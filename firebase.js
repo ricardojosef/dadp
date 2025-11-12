424a42de54dedbee7ac535e155f836ba65ddd1e2
ricardojosef <ricardojcf01@gmail.com>
2025-11-12 11:51:27 -0300

    Pedidos numerados

--- firebase.js
+++ firebase.js
@@ -1,8 +1,17 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
-import { getDatabase, ref, push, set, onValue, update, remove } 
-  from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  set,
+  onValue,
+  update,
+  remove,
+  get,
+} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
 
+// 🔧 Configuração do seu projeto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyA2JDGYzXPl8DxRwL2ER-418wLUerkqnPY",
   authDomain: "dp-251.firebaseapp.com",
@@ -11,9 +20,12 @@ const firebaseConfig = {
   storageBucket: "dp-251.firebasestorage.app",
   messagingSenderId: "52827715825",
   appId: "1:52827715825:web:141832ebb899519c20d150",
-  measurementId: "G-7TZ2RZ2N5P"
+  measurementId: "G-7TZ2RZ2N5P",
 };
 
+// 🔗 Inicializa o app e o banco
 export const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
-export { ref, push, set, onValue, update, remove };
+
+// 🔄 Exporta funções úteis
+export { ref, push, set, onValue, update, remove, get };
